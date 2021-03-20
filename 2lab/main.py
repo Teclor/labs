90500b222cf9e00630f7ff3e817b78398b503676
Antony <teclorian@gmail.com>
2021-03-20 03:26:15 +0300

    ai2

--- 2lab/main.py
+++ 2lab/main.py
@@ -2,11 +2,67 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 import sklearn.datasets
+import pandas as pd
 
-sns.set()
-plt.figure(figsize=(16, 9))
-# housing2 = pd.read_csv("housing.csv")
-housing = sklearn.datasets.fetch_california_housing()
+# sns.set()
+# plt.figure(figsize=(16, 9))
+
+
+def flip_dataframe(df):
+    df = df.T.groupby(level=0).agg(lambda x: x.values.tolist()).stack().apply(pd.Series).unstack().sort_index(level=1, axis=1)
+    df.columns = df.columns.droplevel(level=0)
+
+
+def prepare_housing_dataset(h):
+    h = h.drop("ocean_proximity")
+    length = h.shape[1]
+    h_prepared = np.ndarray([
+        ["data", np.empty(shape=[length, 8], dtype=float)],
+        ["target", np.empty(shape=length, dtype=float)],
+        ["names", np.array([
+            "median_income",
+            "housing_median_age",
+            "average_room_count",
+            "average_bedroom_count",
+            "population",
+            "average_occupation",
+            "latitude",
+            "longitude",
+        ])
+        ]
+    ])
+    for i in range(length):
+        row = h[i].astype(float)
+        h_prepared["target"][i] = row["median_house_value"] / 100000
+        h_prepared["data"][i] = np.array([
+            row["median_income"],
+            row["housing_median_age"],
+            row["total_rooms"] / row["households"],
+            row["total_bedrooms"] / row["households"],
+            row["population"],
+            row["population"] / row["households"],
+            row["latitude"],
+            row["longitude"] if (row["longitude"] >= 0) else (row["longitude"] + 360),
+        ], dtype=float)
+    return h_prepared
+
+housing2 = sklearn.datasets.fetch_california_housing()
+housing = pd.read_csv("housing_flipped.csv", index_col=0)
+# housing = housing.T.groupby(level=0).agg(lambda x: x.values.tolist()).stack().apply(pd.Series).unstack().sort_index(level=1, axis=1)
+# housing.columns = housing.columns.droplevel(level=0)
+# housing.to_csv(path_or_buf="housing_flipped.csv")
+print(housing2.feature_names)
+print(housing2.data[0])
+print(housing2.target[0])
+
+housing.columns = housing.columns.astype(int)
+prepare_housing_dataset(housing)
+print(housing.names)
+print(housing.data[0])
+print(housing.target[0])
+
+
+"""
 from sklearn.naive_bayes import MultinomialNB
 model = MultinomialNB()
 for i in housing.data:
@@ -25,3 +81,4 @@ plt.ylabel('predicted label')
 X_new = [[8.3252, 41, 6.98412698, 1.02380952, 322,
          2.55555556, 37.88, 237.77]]
 print(model.predict(X_new)[0])
+"""
--- 2lab/test.py
+++ 2lab/test.py
@@ -1,3 +1,14 @@
+import pandas as pd
+import numpy as np
+length = 10
+a = np.array([
+       0/3,
+       1/2,
+       2 if True else 3
+]
+)
+print(np.empty(shape=[length, 8], dtype=float))
+"""
 import numpy as np
 rng = np.random.RandomState(1)
 X = rng.uniform(5, size=(6, 100))
@@ -7,3 +18,4 @@ from sklearn.naive_bayes import MultinomialNB
 clf = MultinomialNB()
 clf.fit(X, y)
 print(clf.predict(X[2:3]))
+"""
