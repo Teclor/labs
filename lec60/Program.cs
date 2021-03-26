133275ed22de6a6b3898c413bc64f707e9f6986c
Teclor <teclorian@gmail.com>
2021-03-26 08:42:11 +0300

    final lec60

--- lec60/Program.cs
+++ lec60/Program.cs
@@ -7,6 +7,7 @@ using System.Text.RegularExpressions;
 
 namespace lec60
 {
+
     public static class StringExtensionMethods
     {
         public static string ReplaceFirst(this string text, string search, string replace)
@@ -23,6 +24,29 @@ namespace lec60
         {
             return pos == 0 ? replace + text.Substring(pos + 1) : text.Substring(0, pos) + replace + text.Substring(pos + 1);
         }
+
+        public static string[] SplitKeepSeparators(this string s, params string[] separators)
+        {
+            if (s == null) throw new ArgumentNullException("s");
+            if (s.Length == 0) return new string[] { s };
+
+            var hash = new HashSet<string>(separators);
+            var list = new List<string>();
+
+            int i = 0;
+            for (int j = 0; j < s.Length; j++)
+            {
+                if (hash.Contains(s[j].ToString()))
+                {
+                    if (j > i) list.Add(s.Substring(i, j - i));
+                    list.Add(Convert.ToString(s[j]));
+                    i = j + 1;
+                }
+            }
+            if (i == 0) return new string[] { s };
+            if (i != s.Length) list.Add(s.Substring(i));
+            return list.ToArray();
+        }
     }
     class Interpreter
     {
@@ -30,7 +54,7 @@ namespace lec60
         const int b = 3;
         const int c = 5;
         const int d = 7;
-        const int e = 11;
+        const int e = 0;
         private static Dictionary<int, Tuple<string, string>> Grammar = new Dictionary<int, Tuple<string, string>>{
             {0, new Tuple<string, string>("AXIOM", "E")},
             {1, new Tuple<string, string>("E", "FXY") },
@@ -97,7 +121,7 @@ namespace lec60
                     tokenIndex = 7;
                     end = true;
                     int xIndex = buffer.IndexOf(Grammar[tokenIndex].Item1[0]);
-                    while (xIndex > -1) 
+                    while (xIndex > -1)
                     {
                         buffer = buffer.ReplaceFirst(Grammar[tokenIndex].Item1, Grammar[tokenIndex].Item2);
                         xIndex = buffer.IndexOf(Grammar[tokenIndex].Item1[0]);
@@ -121,7 +145,7 @@ namespace lec60
                 {
                     error = true;
                     errorText = "Syntax error: unexpected token \"" + inputString[index].ToString() + "\"" + " at " + index.ToString();
-                } 
+                }
                 else
                 {
                     buffer = buffer.ReplacePos(getFirstTerminalIndex(), Grammar[tokenIndex].Item2);
@@ -141,6 +165,10 @@ namespace lec60
             {
                 DoActions();
                 DebugPrint(output);
+                output = replaceVariables(output);
+                DebugPrint(output);
+                double res = reversePolishCount(output);
+                Console.WriteLine("RESULT: " + res);
             }
         }
 
@@ -210,7 +238,7 @@ namespace lec60
 
         private void DoActions()
         {
-            string[] methodNames = {"A1", "A2", "A3"};
+            string[] methodNames = { "A1", "A2", "A3" };
             Dictionary<string, MethodInfo> methods = new Dictionary<string, MethodInfo>();
             foreach (string method in methodNames)
             {
@@ -224,8 +252,8 @@ namespace lec60
             while (currentActionIndex > -1)
             {
                 string methodName = bufferPart.Substring(currentActionIndex + 1, 2);
-                methods[methodName].Invoke(this, new object[] { currentActionIndex + (buffer.Length - bufferPart.Length)});
-                bufferPart = bufferPart.Substring(currentActionIndex+1);
+                methods[methodName].Invoke(this, new object[] { currentActionIndex + (buffer.Length - bufferPart.Length) });
+                bufferPart = bufferPart.Substring(currentActionIndex + 1);
                 currentActionIndex = bufferPart.IndexOf('<');
             }
         }
@@ -261,13 +289,118 @@ namespace lec60
             }
         }
 
+        static string replaceVariables(string str)
+        {
+            char[] delimeters = { 'a', 'b','c','d','e'};
+            Dictionary<char, string> vars = new Dictionary<char, string>
+            {
+                { 'a', a.ToString() },
+                { 'b', b.ToString() },
+                { 'c', c.ToString() },
+                { 'd', d.ToString() },
+                { 'e', e.ToString() }
+            };
+            string tmpStr = "";
+            bool found = true;
+            tmpStr = str;
+            while (found)
+            {
+                found = false;
+                foreach (char delim in delimeters)
+                {
+                    int delimPos = tmpStr.IndexOf(delim);
+                    if (delimPos > -1)
+                    {
+                        if (vars[delim] == "")
+                        {
+                            throw new Exception("Переменная " + delim + " присутствует в выражении " + str + " но не определена или не задана");
+                        }
+                        found = true;
+                        tmpStr = tmpStr.Substring(0, delimPos) + vars[delim] + ((delimPos < tmpStr.Length - 1) ? tmpStr.Substring(delimPos + 1) : "");
+                    }
+                }
+            }
+            return tmpStr;
+        }
+
+        static double reversePolishCount(string inStr)
+        {
+            if (inStr == "")
+            {
+                return 0;
+            }
+            Stack<string> st = new Stack<string>();
+            Dictionary<string, ushort> priorities = new Dictionary<string, ushort>
+            {
+                ["/"] = 2,
+                ["*"] = 2,
+                ["+"] = 1,
+                ["-"] = 1,
+                ["("] = 0
+
+            }; // приоритет операций
+            string opsPatt = @"[\+\-\*\/\(\)]?"; // для проверки операторов
+            Regex operators = new Regex(opsPatt);
+            double num;
+            Stack<double> stOut = new Stack<double>();
+            string[] outArr = new string[inStr.Length];
+            for (int i = 0; i < inStr.Length; i++)
+            {
+                outArr[i] = inStr[i].ToString();
+            }
+            for (int i = 0; i < outArr.Length; i++)
+            {
+                if (double.TryParse(outArr[i], out num))
+                    stOut.Push(num);
+                else
+                {
+                    double op2;
+                    switch (outArr[i])
+                    {
+                        case "+":
+                            stOut.Push(stOut.Pop() + stOut.Pop());
+                            break;
+                        case "*":
+                            stOut.Push(stOut.Pop() * stOut.Pop());
+                            break;
+                        case "-":
+                            op2 = stOut.Pop();
+                            if (stOut.Count > 0)
+                            {
+                                stOut.Push(stOut.Pop() - op2);
+                            }
+                            else
+                            {
+                                stOut.Push(-1 * op2);
+                            }
+                            break;
+                        case "/":
+                            op2 = stOut.Pop();
+                            if (op2 != 0.0)
+                                stOut.Push(stOut.Pop() / op2);
+                            else
+                                Console.WriteLine("Ошибка. Деление на ноль");
+                            break;
+                        default:
+                            Console.WriteLine("Ошибка. Неизвестная команда - " + outArr[i]);
+                            break;
+                    }
+                }
+            }
+            return stOut.Pop();
+        }
+
+
     }
     class Program
     {
         static void Main(string[] args)
         {
             string expression = "(-a+b)*(c+d)";
+            //expression = "-a+c*(d-e)/a*d+b-c/b*(a+(b-a/d))";
+            expression = "((a+b)*c+d)*a";
             Console.WriteLine("На входе получено: " + expression);
+            //Interpreter.PrintGrammar();
             Interpreter interpreter = new Interpreter(expression, true);
         }
     }
